a59ff1f111aa2a94ad2344c8bf619926b7fc4891
DimaVnuk <vnuchok311994@hotmail.com>
2021-02-04 22:27:29 +0300

    add start css modules

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css';
+const Navbar = () => {
+  return <nav className="nav">
+    <div className="nav_item"><a src="#">Profile</a></div>
+    <div className="nav_item"><a src="#">Messages</a></div>
+    <div className="nav_item"><a src="#">News</a></div>
+    <div className="nav_item"><a src="#">Music</a></div>
+    <div className="nav_item"><a src="#">Settings</a></div>
+  </nav>
+}
+
+export default Navbar;
